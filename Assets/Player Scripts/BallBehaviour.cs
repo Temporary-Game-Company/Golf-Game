9047898011a438877cf2394a22d9997bf41d5310
Joshua-E-Weinstein <76671618+Joshua-E-Weinstein@users.noreply.github.com>
2021-06-11 02:22:55 -0400

    Changed Bounces

--- Assets/Player Scripts/BallBehaviour.cs
+++ Assets/Player Scripts/BallBehaviour.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BallBehaviour : MonoBehaviour // Defines useful behaviours for the ball.
 {
     private Rigidbody2D rb2D;
 
+    private Boolean launched = false;
+
     public int bounceCount;
-    private int MAX_BOUNCES = 3;
+    private int MAX_BOUNCES = 10;
 
     public BallController player; // BallController passes itself to this object.
 
@@ -26,19 +29,26 @@ public class BallBehaviour : MonoBehaviour // Defines useful behaviours for the 
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        bounceCount++;
 
-        if (bounceCount >= MAX_BOUNCES)
+        if (collision.collider.gameObject.name != "Enemy" && launched == true)
+        { 
+            bounceCount++;
+        }
+
+        if (bounceCount >= MAX_BOUNCES || (rb2D.velocity.magnitude <= 2f))
         {
             player.CreateBall(); // TEMPORARY: immediately creates a new ball when the current one is destroyed. Method found in BallController.cs
-            Object.Destroy(gameObject); // Banishes this ball instance
+            Destroy(gameObject); // Banishes this ball instance
         }
     }
 
-    public void Launch(Quaternion direction, float force) // Launches the ball. Normally called from BallController.cs by the player game object.
+    public async void Launch(Quaternion direction, float force) // Launches the ball. Normally called from BallController.cs by the player game object.
     {
         rb2D.transform.rotation = direction; // Points the ball towards the launch direction.
         rb2D.WakeUp(); // Turns on physics. Might be redundant?
         rb2D.AddForce(transform.right * force); // Launches the ball in the direction it is facing.
+
+        await System.Threading.Tasks.Task.Delay(1000);
+        launched = true;
     }
-}
+}   
