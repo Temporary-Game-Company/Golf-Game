0ee00014350f47b173925456983386de95ab69f9
Joshua-E-Weinstein <76671618+Joshua-E-Weinstein@users.noreply.github.com>
2021-06-13 21:57:25 -0400

    Debug Button + Canvas Fixed

--- Assets/Player Scripts/EnemyBehaviour.cs
+++ Assets/Player Scripts/EnemyBehaviour.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     public int health;
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -20,5 +21,10 @@ public class EnemyBehaviour : MonoBehaviour
             Debug.Log(1);
             Destroy(gameObject);
         }
+    }
+
+    public void Approach()
+    {
+        Debug.Log(player.transform.position);
     }
 }
