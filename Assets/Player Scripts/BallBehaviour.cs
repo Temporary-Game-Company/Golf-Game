32c1072300eb4e6aa13c46d29734cfc16e227e83
TheFishFlopper <abarbeau14@gmail.com>
2021-07-26 16:59:25 -0400

    New Branch

---
+++ Assets/Player Scripts/BallBehaviour.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading.Tasks;
+
+
+public class BallBehaviour : MonoBehaviour // Defines useful behaviours for the ball.
+{
+    private Rigidbody2D rb2D;
+    public Boolean launched;
+    private int bounceCount;
+    public double comboCount;
+
+    private BallController ballController; // BallController passes itself to this object.
+    private BattleManager battleManager;
+
+    private const float poofTime = 0.6f;
+    private float slowTimer = poofTime;
+    private Animator animator;
+    public bool destroyed;
+
+    private TrailRenderer trail;
+    private Material trailMat;
+    private Color TempColor;
+    private GradientAlphaKey[] tempAlphaGradient;
+    private Gradient tempGradient;
+    private float alphaCoefficient;
+
+    // debugInfo is used only to send data to the debug overlay
+    private DebugDisplay debugInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+
+        ballController = GameObject.Find("BattleManager").GetComponent<BallController>();
+
+        animator = GetComponent<Animator>();
+
+        trail = GetComponent<TrailRenderer>();
+        trailMat = trail.material;
+
+        // debugInfo is used only to send data to the debug overlay
+        debugInfo = GameObject.Find("Debug Overlay").GetComponent<DebugDisplay>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destroyed)
+        {
+            return;
+        }
+        
+        slowTimer -= Time.deltaTime;
+
+        if (rb2D.velocity.magnitude <= 2f && launched)
+        {
+            if (slowTimer <= 0)
+            {
+                Poof();
+            }
+        }
+        else
+        {
+            slowTimer = poofTime;
+        }
+
+        // Updates trail opacity and length
+        TempColor = trailMat.color;
+        TempColor.a = (float) (0.75*(1 - Math.Pow(1.2, -rb2D.velocity.magnitude + 4)));
+        trailMat.color = TempColor;
+    
+        tempGradient = new Gradient();
+        tempGradient.SetKeys(
+            trail.colorGradient.colorKeys,
+            new GradientAlphaKey[] { new GradientAlphaKey(1f, 0f), new GradientAlphaKey(0f, (float) (rb2D.velocity.magnitude/(2*Math.Sqrt(1 + (rb2D.velocity.magnitude-6) * (rb2D.velocity.magnitude-6)))+0.5))}
+        );
+        
+        trail.colorGradient = tempGradient;
+
+        // Updates debug info
+        debugInfo.UpdateDebug("Ball Speed", rb2D.velocity.magnitude.ToString());
+        debugInfo.UpdateDebug("Ball Velocity", rb2D.velocity.ToString());
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        bounceCount += 1;
+        if (collision.collider.gameObject.name == "Outer Box")
+        {
+            DestroyBall();
+        }
+
+        else if (collision.collider.gameObject.tag == "Enemy")
+        {
+            collision.collider.gameObject.GetComponent<EnemyBehaviour>().TakeDamage((int) (50d * (Math.Pow(1.42f, comboCount)) ) );
+            comboCount += 1;
+        }
+
+        else if (comboCount > 0)
+        {
+            comboCount -= 1;
+        }
+        
+        if (bounceCount >= 6)
+        {
+            Poof();
+        }
+
+        UpdateTrail();
+    }
+
+    public async void Launch(Quaternion direction, float force) // Launches the ball. Normally called from BallController.cs by the player game object.
+    {
+        launched = true;
+        rb2D.transform.rotation = direction; // Points the ball towards the launch direction.
+        rb2D.WakeUp(); // Turns on physics. Might be redundant?
+        rb2D.AddForce(transform.right * force); // Launches the ball in the direction it is facing.
+
+        await Task.Delay(1000);
+    }
+
+    private async void Poof()
+    {
+        destroyed = true;
+
+        
+        animator.SetBool("Poofing", true);
+
+        await WaitForAnimation();
+        
+        DestroyBall();
+    }
+
+    private void DestroyBall()
+    {
+        Destroy(gameObject); // Banishes this ball instance
+        ballController.ResetValues();
+    }
+
+    private async Task WaitForAnimation() // Task which waits for animation to end.
+    {
+        await Task.Delay(300);
+        rb2D.drag = 15;
+        while (animator.GetBool("Poofing"))
+        {
+            await Task.Delay(25);
+        }
+    }
+
+    private void UpdateTrail() // Updates the color of the ball's trail to reflect the current combo
+    {
+        switch(comboCount)
+        {
+            case 0: TempColor = new Color (1f, 1f, 1f); break;
+            case 1: TempColor = new Color (1f, 0.9f, 0.36f);break;
+            case 2: TempColor = new Color (1f, 0.56f, 1f); break;
+            default: TempColor = new Color (1f, 1f, 1f); break;
+        }
+        
+        tempGradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(TempColor, 0f), new GradientColorKey(TempColor, 1f)},
+            trail.colorGradient.alphaKeys
+        );
+        trail.colorGradient = tempGradient;
+    }
+}   
---
+++ Assets/Player Scripts/BallController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour // Used by the player to create and launch balls. Also handles player input.
+{
+    public GameObject ball;
+    public GameObject ballTemplate; // Defined in the Unity editor: is instanced in order to create new balls
+    public BallBehaviour launcher;
+    private GameObject bar;
+    private GameObject ui;
+    private GameObject arrow;
+
+    public int state = 0; // 0 when the angle is being selected, 1 while the power is being selected, 2 after power is selected, 3 after the ball has been launched
+    [Range(0, 359)]
+
+    private int incrementDirection = 1; // Used by IncrementAngle and IncrementForce to change direction.
+
+    public bool ballExists;
+
+    public float MIN_ANGLE;
+    public float MAX_ANGLE;
+    public float launchAngle;
+    public Quaternion launchDirection;
+    
+    private float MIN_FORCE;
+    public float MAX_FORCE;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = GameObject.Find("Power Bar");
+
+        arrow = GameObject.Find("Arrow");
+        
+        ResetValues(); // Technically it's setting some of these for the first time
+    }
+
+    // Resets important variables prior to creating a new ball
+    public void ResetValues()
+    {
+        state = 0;
+        launchAngle = 0f;
+        force = 0f;
+
+        MIN_ANGLE = 0f;
+        MAX_ANGLE = 90f;
+        MIN_FORCE = 1f;
+        MAX_FORCE = 210f;
+
+        arrow.SetActive(false);
+        bar.SetActive(false);
+
+        ballExists = false;
+    }
+
+    // Creates a new ball to be launched.
+    public void CreateBall()
+    {
+        ballExists = true;
+        GameObject newBall = Object.Instantiate(ballTemplate); // Temp variable
+        Bind(newBall); // Makes sure the script actually uses the new ball
+
+        arrow.SetActive(true);
+    }
+
+    // Sets all variables which call on the ball object to use the newly created ball
+    void Bind(GameObject boundBall)
+    {
+        ball = boundBall;
+        launcher = ball.GetComponent<BallBehaviour>();
+    }
+
+    // Fixed update is called at a fixed rate
+    void FixedUpdate()
+    {
+        if (state == 0) // Angle is being selected
+        {
+            IncrementAngle();
+        }
+
+        if (state == 1) // Power is being selected
+        {
+            IncrementForce();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        launchDirection = Quaternion.AngleAxis(launchAngle, Vector3.forward).normalized;
+
+        // used to get player input
+        if (Input.GetKeyDown("space") && ballExists)
+        {
+            if (state < 2) // select angle and force
+            {
+                state++;
+                if (state == 1) // Power State
+                {
+                    bar.SetActive(true);
+                }
+                else // Launch State
+                {
+                    arrow.SetActive(false);
+                    bar.SetActive(false);
+                }
+            }
+
+            if (state == 2 && !launcher.launched) // actually launch the ball
+            {
+                launcher.Launch(launchDirection, force); // See BallBehavior.cs for how this works
+            }
+        }
+        
+    }
+
+    // Makes the angle bounce back and forth. For the arrow's behaviour see ArrowAngle.cs
+    void IncrementAngle()
+    {
+        if (launchAngle <= MIN_ANGLE)
+        {
+            incrementDirection = 1;
+        }
+        if (launchAngle >= MAX_ANGLE)
+        {
+            incrementDirection = -1;
+        }
+        launchAngle += 3 * incrementDirection;
+    }
+
+    // Makes the force bounce back and forth. For the meter's behaviour see PowerBar.cs
+    void IncrementForce()
+    {
+        if (force <= MIN_FORCE)
+        {
+            incrementDirection = 1;
+        }
+
+        if (force >= MAX_FORCE)
+        {
+            incrementDirection = -1;
+        }
+
+        force += 5 * incrementDirection;
+
+    }
+}
